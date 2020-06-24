88e764bf44aa7f4d578d876732ca6bd686a3f91d
Jeremy Huot <logflume@live.com>
2020-06-24 20:59:52 +0000

    Added countLetters.js function to return each number of characters in a string

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const countLetters = function(sentence) {
+  let result = {};
+  let s2 = sentence.replace(/\s+/g, '');
+  for (let i of s2) {
+      if (result[i]) {
+        result[i] += 1;
+      } else {
+        result[i] = 1;
+      }
+  }
+  console.log(result);
+  return result;
+};
+
+
+countLetters("lighthouse in the house");
