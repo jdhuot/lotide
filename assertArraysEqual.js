1b8cb844774eb7056f13feb4820fb4dd04b0e795
Jeremy Huot <logflume@live.com>
2020-06-24 01:18:13 +0000

    added function to test array equality

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const assertArraysEqual = function(arr1, arr2) {
+  let status;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        status = false;
+        break;
+      } else {
+        status = true;
+      }
+    }
+  } else {
+    status = false;
+  }
+  if (status === true) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+// TEST CODE
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+// assertArraysEqual([1, "2", 3], [1, 2, 3]); // => should FAIL
---
+++ eqArrays.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  let status = true;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return status = false;
+      } else {
+        status = true;
+      }
+    }
+  } else {
+    return false;
+  }
+  return status;
+};
+
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, "2", 3], [1, 2, 3]), false); // => should FAIL
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2",3]), false); // => false
+
