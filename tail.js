8700d040e39cc7bdc12c696ce10e3f210cc4d29f
Jeremy Huot <logflume@live.com>
2020-06-29 15:40:53 +0000

    Refactored tail function to require module and separated tests

--- tail.js
+++ tail.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 
 const tail = function(arr) {
@@ -12,17 +5,6 @@ const tail = function(arr) {
   return newArr;
 };
 
-// TEST CODE
-let test1 = tail([1,"banana","apple",4,5]);
 
-assertEqual(test1.length,4);
-assertEqual(test1[0],"banana");
-assertEqual(test1[1],"apple");
+module.exports = tail;
 
-
-
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
---
+++ test/tailTest.js
@@ -0,0 +1,18 @@
+const tail = require("../tail");
+const assertEqual = require("../assertEqual");
+
+
+// TEST CODE
+let test1 = tail([1,"banana","apple",4,5]);
+
+assertEqual(test1.length,4);
+assertEqual(test1[0],"banana");
+assertEqual(test1[1],"apple");
+
+
+
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
