45a90dbeb175eb44f0d11004b852fbbbe9a37ee2
Jeremy Huot <logflume@live.com>
2020-06-29 22:08:55 +0000

    Added index.js to require and export all our functions

---
+++ index.js
@@ -0,0 +1,35 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
+const findKey = require('./findKey');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const countLetters = require('./countLetters');
+const eqObjects = require('./eqObjects');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const middle = require('./middle');
+
+
+
+module.exports = {
+  head,
+  tail,
+  assertEqual,
+  assertArraysEqual,
+  eqArrays,
+  findKey,
+  letterPositions,
+  findKeyByValue,
+  countLetters,
+  eqObjects,
+  map,
+  takeUntil,
+  without,
+  assertObjectsEqual,
+  middle,
+};
